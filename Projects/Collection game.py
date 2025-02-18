255f4cd5edf915022add15715f4131946b1fae66
Coconut-13 <Ivylorca@seattleacademy.org>
2025-02-18 02:20:52 +0000

    hand

--- Projects/Collection game.py
+++ Projects/Collection game.py
@@ -1,6 +1,17 @@
-#Charecters & background
+# Charecters & background
 player = codesters.Sprite("gum packet")
-object = codesters.Sprite("gum")
-object = codesters.Sprite("hands")
-stage.set_background("school")
 
+object = codesters.Sprite("gum")
+object = codesters.Sprite("hand")
+
+stage = StageClass()
+set_background("school")
+
+
+
+# Controls 
+
+player.event_key("Up arrow", up)
+player.event_key("Down arrow", down)
+player.event_key("Right arrow", right)
+player.event_key("Left arrow", left)
